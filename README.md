8910ae54c19e29ab43c93bffd69f953d140c5a11
MrV01 <VladV454@gmail.com>
2017-07-18 02:23:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+
+1. λ npm install express@4.15.3 --save
+
+2. λ nodemon server.js
+
+3. http://localhost:3000
+
+4. HandlebarsJS  vie engine:
+    λ npm install hbs@4.0.1 --save
+
+5. Start nodemon , which is watching additional extentions of files  .js AND  .hbs AND html:
+  λ nodemon server.js -e js,hbs,html  
+
+6. Git  . Adding Version Control  to the project.
+
+6.1.  λ git init
+Initialized empty Git repository in C:/Users/Vlad/Documents/PROG/HTML5/sites/completeNodeJsDeveloperCourse2/node-web-server/.git/
+
+C:\Users\Vlad\Documents\PROG\HTML5\sites\completeNodeJsDeveloperCourse2\node-web-server (master) (node-web-server@1.0.0)
+
+6.2.  λ git status
+On branch master
+
+Initial commit
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+
+        README.md
+        node_modules/
+        package-lock.json
+        package.json
+        public/
+        server.js
+        server.log
+        views/
+
+nothing added to commit but untracked files present (use "git add" to track)
---
+++ server.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//
+// Using template language, And template engine.
+//  handlebarsjs.com  Handlebars ( Other options are: EJS, pug)
+//  npm hbs  @ https://www.npmjs.com/package/hbs
+//  It is Express.js view engine for handlebars.js
+//
+hbs.registerPartials(__dirname + '/views/partials');  // Add support of partials to the HBS engine
+// added footer.hbs  by Instructor
+// Challenge 3.  add header.hbs
+app.set('view engine', 'hbs');
+
+
+// Register  Third  party  middlewaree in Express
+// For instance logging requests to a log file
+//
+app.use((req, res, next ) => {  // request, response, next function to continue
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log +'\n', (err) => {
+    if(err){
+      console.log("Unable to append to server.log");
+    }
+  });
+  next(); // Middleware must call next() pice of middleware . Or app.js will hangs in beetwwen of app.use-es
+  // Output example
+  // Node server.js ExpressJS Server is up on port 3000
+  // Tue Jul 18 2017 01:37:05 GMT-0400 (Eastern Daylight Time): GET /
+  // Tue Jul 18 2017 01:37:10 GMT-0400 (Eastern Daylight Time): GET /about
+  // Tue Jul 18 2017 01:37:30 GMT-0400 (Eastern Daylight Time): GET /
+});
+
+// Challege 3.  Set up middleware, which will show maintenece page only. and nothing else.
+// ( In Express object. )
+app.use(( req, res, next) => {
+  res.render('maintenance.hbs'); // renders responce page
+});
+// next() call is absent, therefore  only one maintenance page will be rendered
+// Next piece of middleware will not get control.
+// Register "Middleware" embedded into Express.js to run static web page:
+app.use(express.static(__dirname + '/public'));
+
+
+// Regietering helper(s)  on Handlebars engine
+// Now we can use the name in .hbs files as: {{getCurrentYear}}
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear()) ;
+// Helper with parameter(s)  .hbs format: {{screamIt welcomeMessage  }}
+hbs.registerHelper('screamIt', (screamText) => {
+  return screamText.toUpperCase();
+});
+
+//
+// Basic server, which is listening http/tcp port 3000 and has response handlers running:
+  // 1.  /
+  // 2.  /bad
+  // 3.  /json
+  // 4.  /about
+  //
+
+
+  //1. Route / (root)
+app.get('/hello', (req, res) => {
+  res.send('<h1>Hello Express!</h1>');
+});
+
+//
+// Challenge 1. :  Send back some JSON, and set up errorMessage property
+// 2. Route /bad  send  JSON with errorMessage propery set to some string.
+app.get('/bad', (req, res) => {
+    res.send({
+        name: 'VVV',
+        likes: [
+          'Cooking',
+          'Biking',
+          'Suburbian'
+        ],
+        errorMessage: 'Bad responce sent to the client'
+  });
+});
+
+// 4.  Route /about   Uses HandlebarJS template ( by default from views folder ).
+  app.get('/about',  (req, res) => {
+      res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+      });
+  });
+
+// Challenge 2. Create Home page with dynamic content.
+// 5.  Route / ( root) modified.   Uses HandlebarJS template ( by default from views folder ).
+  app.get('/',  (req, res) => {
+      res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the Hymalaya!!!! (Web site)',
+        currentYear: new Date().getFullYear()
+      });
+  });
+
+// 3.  Route JSON response
+    app.get('/json', (req, res) => {
+
+      res.send({
+          name: 'VVV',
+          likes: [
+            'Cooking',
+            'Biking',
+            'Country'
+          ]
+      });
+
+    });
+
+app.listen(3000, () => console.log("Node server.js ExpressJS Server is up on port 3000"));
